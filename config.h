c13eda3e2be61169bde80dd463b75fe2a826aafc
Gavinok <gavinfreeborn@gmail.com>
2019-04-11 18:06:15 -0600

    realigned

--- config.h
+++ config.h
@@ -212,7 +212,7 @@ static Key keys[] = {
     { MODKEY|GDK_SHIFT_MASK, 	 GDK_KEY_p,      print,				 { 0 } },
     { MODKEY,                	 GDK_KEY_t,      newwindow,			 { 0 } },
     { MODKEY|GDK_SHIFT_MASK,	 GDK_KEY_a,      togglecookiepolicy, { 0 } },
-    { 0,						 GDK_KEY_F11,    togglefullscreen,   { 0 } },
+    { 0,			 GDK_KEY_F11,    togglefullscreen,   { 0 } },
     { MODKEY|GDK_SHIFT_MASK,	 GDK_KEY_i,      toggleinspector,    { 0 } },
 
     { MODKEY,				 	 GDK_KEY_v,      toggle,     { .i = CaretBrowsing } },
