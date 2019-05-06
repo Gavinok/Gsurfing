b4ef4b3820fcbd30f197c3aef73732e0e71ac1ed
Gavinok <gavinfreeborn@gmail.com>
2019-05-05 22:36:33 -0600

    Realigned start of config.h

--- config.h
+++ config.h
@@ -1,12 +1,13 @@
 /* modifier 0 means no modifier */
-static int surfuseragent    = 1;  /* Append Surf version to default WebKit user agent */
-static char *fulluseragent  = ""; /* Or override the whole user agent string */
-static char *scriptfile     = "~/.surf/script.js";
-static char *styledir       = "~/.surf/styles/";
-static char *certdir        = "~/.surf/certificates/";
-static char *cachedir       = "~/.surf/cache/";
-static char *cookiefile     = "~/.surf/cookies.txt";
-static char *dmenuhandlerpath = "/home/gavinok/.scripts/dmenu/dmenuhandler";
+static int surfuseragent	= 1;  /* Append Surf version to default WebKit user agent */
+static char *fulluseragent	= ""; /* Or override the whole user agent string */
+static char *scriptfile		= "~/.surf/script.js";
+static char *styledir		= "~/.surf/styles/";
+static char *certdir		= "~/.surf/certificates/";
+static char *cachedir		= "~/.surf/cache/";
+static char *cookiefile		= "~/.surf/cookies.txt";
+static char *dmenuhandlerpath	= "/home/gavinok/.scripts/dmenu/dmenuhandler";
+static char *linkhandlerpath	= "/home/gavinok/.scripts/linkhandler";
 /* Webkit default features */
 /* Highest priority value will be used.
  * Default parameters are priority 0
